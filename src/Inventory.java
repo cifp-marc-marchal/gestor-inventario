48f8c40c0e8ac165baf828df1098ed2ae3fed5ab
Marc Marchal <marcmarchalpastor@a.cifpperedesongall.org>
2025-01-28 19:22:44 +0100

    Function to insert products to the inventory

--- src/Inventory.java
+++ src/Inventory.java
@@ -1,3 +1,10 @@
+import java.util.Arrays;
+
 public class Inventory {
     String[] inventory = {"Ordenador", "Mesa", "Silla", "Pantalla", "Lapiz", "Goma", "Teclado", "Raton"};
+
+    public void insertProduct(String product) {
+        this.inventory = Arrays.copyOf(inventory, inventory.length + 1);
+        this.inventory[inventory.length - 1] = product;
+    }
 }
