b2366c61dc043d7a6aabea5ba7e432564c9ad9ba
Joan <joanbonetpuig@gmail.com>
2025-01-28 20:10:50 +0100

    Mostrar inventario completo con las cantidades.

--- src/Inventory.java
+++ src/Inventory.java
@@ -1,3 +1,29 @@
+import java.util.Arrays;
+
 public class Inventory {
     String[] inventory = {"Ordenador", "Mesa", "Silla", "Pantalla", "Lapiz", "Goma", "Teclado", "Raton"};
+
+    public void mostrar() {
+        if (inventory.length == 0) {
+            System.out.println("El inventario esta vacio");
+        } else {
+            String[] noRepe;
+            noRepe = Arrays.copyOf(inventory, inventory.length);
+            for (int i = 0; i < noRepe.length; i++) {
+                int cont = 0;
+                if (!noRepe[i].equalsIgnoreCase(" ")) {
+                    for (int j = 0; j < noRepe.length; j++) {
+                        if (noRepe[i].equals(noRepe[j])) {
+                            cont++;
+                            if (cont != 1) {
+                                noRepe[j] = " ";
+                            }
+                        }
+                    }
+                    System.out.print(noRepe[i] + " ");
+                    System.out.println(cont);
+                }
+            }
+        }
+    }
 }
