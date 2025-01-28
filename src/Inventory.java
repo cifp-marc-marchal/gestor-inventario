a32be19175dcaaad0b0968c28c8dc2e65e49c077
mbpCGFP1 <miquelbonetpuig@a.cifpperedesongall.org>
2025-01-28 20:00:37 +0100

    Funcion eliminar productos por nombre.

--- src/Inventory.java
+++ src/Inventory.java
@@ -1,3 +1,16 @@
 public class Inventory {
     String[] inventory = {"Ordenador", "Mesa", "Silla", "Pantalla", "Lapiz", "Goma", "Teclado", "Raton"};
+    public void eliminar(String nombre) {
+        String[] eliminado = new String[inventory.length - 1];
+        if (nombre.equals("")) {
+        }else {
+            for (int i = 0; i < this.inventory.length; i++) {
+                if (this.inventory[i].toLowerCase().equals(nombre.toLowerCase())) {
+                    System.arraycopy(this.inventory,0, eliminado,0, i);
+                    System.arraycopy(this.inventory,i+1, eliminado,i, this.inventory.length - i -1);
+                    this.inventory = eliminado;
+                }
+            }
+        }
+    }
 }
